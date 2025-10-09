08a90c4d8177805a8202581ce84810f2d8bc154e
Saad Mirza <121344315+icas00@users.noreply.github.com>
2025-10-09 22:51:21 +0530

    Update OrderResponseDTO.java

--- src/main/java/grocery/dto/OrderResponseDTO.java
+++ src/main/java/grocery/dto/OrderResponseDTO.java
@@ -7,9 +7,7 @@ import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.List;
 
-/**
- * DTO for order response with order details and items.
- */
+
 @Data
 public class OrderResponseDTO {
 
