0f09bffa442c877f735e4cf26416880f18a1367c
Saad Mirza <121344315+icas00@users.noreply.github.com>
2025-10-09 22:52:38 +0530

    Update CustomException.java

--- src/main/java/grocery/exception/CustomException.java
+++ src/main/java/grocery/exception/CustomException.java
@@ -1,8 +1,6 @@
 package grocery.exception;
 
-/**
- * Custom exception class for application-specific exceptions.
- */
+
 public class CustomException extends RuntimeException {
 
     public CustomException(String message) {
