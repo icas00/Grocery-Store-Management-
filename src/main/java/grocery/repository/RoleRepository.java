8c5d6fc1c7d3811a269228c2f15136e755a4bb09
Saad Mirza <121344315+icas00@users.noreply.github.com>
2025-10-09 22:54:15 +0530

    Update RoleRepository.java

--- src/main/java/grocery/repository/RoleRepository.java
+++ src/main/java/grocery/repository/RoleRepository.java
@@ -4,9 +4,7 @@ import grocery.entity.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
-/**
- * Repository interface for Role entity.
- */
+
 public interface RoleRepository extends JpaRepository<Role, Long> {
     Optional<Role> findByName(String name);
 }
