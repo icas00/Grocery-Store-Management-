3fddcee4a96b64500ea98db543f6d65f4b7e58b8
Saad Mirza <121344315+icas00@users.noreply.github.com>
2025-10-09 22:50:29 +0530

    Update AuthResponse.java

--- src/main/java/grocery/dto/AuthResponse.java
+++ src/main/java/grocery/dto/AuthResponse.java
@@ -3,9 +3,7 @@ package grocery.dto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
-/**
- * DTO for login response containing JWT token.
- */
+
 @Data
 @AllArgsConstructor
 public class AuthResponse {
