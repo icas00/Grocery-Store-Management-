ca0fe5fc5b022df97d063c29793ff48eb052d338
Saad Mirza <121344315+icas00@users.noreply.github.com>
2025-10-09 22:54:01 +0530

    Update ProductRepository.java

--- src/main/java/grocery/repository/ProductRepository.java
+++ src/main/java/grocery/repository/ProductRepository.java
@@ -5,8 +5,5 @@ package grocery.repository;
 import grocery.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-/**
- * Repository interface for Product entity.
- */
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
