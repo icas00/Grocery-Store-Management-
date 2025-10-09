1fcfe92f033cc9e18d7f1f90386421cb6b8ff002
Saad Mirza <121344315+icas00@users.noreply.github.com>
2025-10-09 22:54:28 +0530

    Update UserRepository.java

--- src/main/java/grocery/repository/UserRepository.java
+++ src/main/java/grocery/repository/UserRepository.java
@@ -5,9 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-/**
- * Repository interface for User entity.
- */
+
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
 
